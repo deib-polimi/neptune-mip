ce28d82cc31d81458eb7d7394d214407c1e8ca89
dragonbanana <davideyi.hu@polimi.it>
2023-05-26 17:24:28 +0200

    Better testing

--- allocation_algorithm_test.py
+++ allocation_algorithm_test.py
@@ -227,8 +227,47 @@ inputs = [
         "actual_gpu_allocations": {
         }
     },
+    # Many node, many functions
+    # None of them were allocated
+    {
+        "solver": {
+            "type": solver_type,
+            "args": {"alpha": 0.0, "verbose": False}
+        },
+        "community": "community-test",
+        "namespace": "namespace-test",
+        "node_names": [f"node_{i}" for i in range(20)],
+        "node_memories": [100 for i in range(20)],
+        "node_cores": [100 for i in range(20)],
+        "gpu_node_names": [],
+        "gpu_node_memories": [],
+        "function_names": [f"ns/fn_{i}" for i in range(5)],
+        "function_memories": [10 for i in range(5)],
+        "function_max_delays": [100 for i in range(5)],
+        "gpu_function_names": [],
+        "gpu_function_memories": [],
+        "actual_cpu_allocations": {
+            # "ns/fn_0": {
+            #     "node_1": True,
+            # },
+            # "ns/fn_1": {
+            #     "node_1": True,
+            # },
+            # "ns/fn_2": {
+            #     "node_1": True,
+            # },
+            # "ns/fn_3": {
+            #     "node_1": True,
+            # },
+            # "ns/fn_4": {
+            #     "node_1": True,
+            # },
+        },
+        "actual_gpu_allocations": {
+        }
+    },
 ]
 
-for input_request in inputs:
+for input_request in inputs[-1:]:
     response = requests.request(method='get', url="http://localhost:5000/", json=input_request)
     pprint.pprint(response.json())
