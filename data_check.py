18cf786d1f2996544ac87442e27f2f6edaf91561
andreasbrummer <andreas.brummer00@gmail.com>
2024-06-24 16:06:31 +0200

    data generation improved and minor changes

---
+++ data_check.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def check_workload(core_per_req_matrix, total_cores , num_functions, num_nodes, workload_on_source_matrix, dataG):
+    cores_needed = 0
+    for i in range(len(core_per_req_matrix)):
+            for j in range(len(core_per_req_matrix[i])):
+                cores_needed += core_per_req_matrix[i][j] * workload_on_source_matrix[i][j]
+    
+    return cores_needed >= total_cores
